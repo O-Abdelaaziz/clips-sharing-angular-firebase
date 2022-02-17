ebaad616029eb58a1f22a0a81d2265a99efa2428
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-02-17 09:47:32 +0100

    Task 35: working with validating form & disabling button

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -10,8 +10,8 @@ export class RegisterComponent implements OnInit {
   public fullName = new FormControl('', [Validators.required, Validators.minLength(3)]);
   public email = new FormControl('', [Validators.required, Validators.email]);
   public age = new FormControl('',[Validators.required,Validators.min(18),Validators.max(50)]);
-  public password = new FormControl('',[Validators.required, Validators.pattern('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm')]);
-  public confirmPassword = new FormControl('',[Validators.required, Validators.pattern('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm')]);
+  public password = new FormControl('',[Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)]);
+  public confirmPassword = new FormControl('',[Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)]);
   public phoneNumber = new FormControl('',[Validators.required,Validators.minLength(13),Validators.maxLength(13)]);
   public registerFromGroup: FormGroup = new FormGroup({
     fullName: this.fullName,
