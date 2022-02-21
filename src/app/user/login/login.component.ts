63a97e0436675cebdf8b7d6c47b5cf60e57d253b
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-02-21 14:23:33 +0100

    Task 57: working with setting up the login form (login user into firebase)

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-login',
@@ -11,13 +12,34 @@ export class LoginComponent implements OnInit {
     password: ''
   };
 
-  constructor() {
+  public showAlert: boolean = false;
+  public alertColor: string = 'blue';
+  public alertContent: string = 'Please wait! we are logging you in...';
+  public inSubmission: boolean = false;
+
+  constructor(private _angularFireAuth: AngularFireAuth) {
   }
 
   ngOnInit(): void {
   }
 
-  onLogin($event: Event) {
-    console.log($event)
+  async onLogin() {
+    this.showAlert = true;
+    this.alertContent = 'Please wait! Your account is being created.';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+
+
+    try {
+      await this._angularFireAuth.signInWithEmailAndPassword(
+        this.credentials.email, this.credentials.password
+      );
+    } catch (e) {
+      console.log(e)
+      this.inSubmission = false;
+
+    }
+    this.alertContent = "Success! Your hav been connected successfully";
+    this.alertColor = "green";
   }
 }
