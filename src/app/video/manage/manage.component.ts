f791ca759f84627da18b4a74eb88a64e6821638f
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-02-26 10:36:56 +0100

    Task 77: working with route parameters & query parameters

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-manage',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage.component.css']
 })
 export class ManageComponent implements OnInit {
+  videoOrder: string = '1';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute
+  ) {
   }
 
+  ngOnInit(): void {
+    this._activatedRoute.queryParamMap.subscribe(
+      (params: Params) => {
+        this.videoOrder = params['sort'] === 2 ? params['sort'] : '1';
+      }
+    )
+  }
+
+  onSortItems(event: Event) {
+    const {value} = (event.target as HTMLSelectElement);
+    this._router.navigateByUrl(`/manage?sort=${value}`);
+  }
 }
