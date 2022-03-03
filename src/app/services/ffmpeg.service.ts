95806f24b0763fce8203943352787c49b8fb06e4
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-03-03 17:29:32 +0100

    Task 115: working with generating a screenshot (multiple screenshots)

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -23,15 +23,25 @@ export class FfmpegService {
   async getScreenshots(file: File) {
     const data = await fetchFile(file);
     this.ffmpeg.FS('writeFile', file.name, data);
+
+    const seconds = [1, 2, 3];
+    const commands: string[] = [];
+
+    seconds.forEach(second => {
+      commands.push(
+        // Input
+        '-i', file.name,
+        // Output Option
+        '-ss', `00:00:0${second}`,
+        '-frames:v', '1',
+        '-filter:v', 'scale=510:-1',
+        //Output
+        `output_0${second}.png`
+      );
+    });
+
     await this.ffmpeg.run(
-      // Input
-      '-i', file.name,
-      // Output Option
-      '-ss', '00:00:01',
-      '-frames:v', '1',
-      '-filter:v', 'scale=510:-1',
-      //Output
-      'output_01.png'
+      ...commands
     )
   }
 }
